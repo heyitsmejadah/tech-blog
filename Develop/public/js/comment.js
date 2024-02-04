964cc474b23ac16091dce66bfe19b5d705a9604d
Jadah Pierce <145732309+heyitsmejadah@users.noreply.github.com>
2024-02-04 07:04:34 -0700

    Added JS logic in public js folder

---
+++ Develop/public/js/comment.js
@@ -0,0 +1,36 @@
+const commentTextEl = document.querySelector("#comment");
+
+const postCommentHandler = async (event) => {
+    event.preventDefault();
+
+    const content = commentTextEl.value.trim();
+
+    if (content.length > 0) {
+        const post_id = document.location.pathname.split("/")[2];
+
+        try {
+            const response = await fetch("/api/comments/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ content, post_id }),
+            });
+
+            if (response.ok) {
+                document.location.reload();
+            } else {
+                throw new Error("Comment could not be saved to the post");
+            }
+        } catch (error) {
+            console.error(error.message);
+            alert("Something went wrong. Please try again later.");
+        }
+    }
+};
+
+const postCommentButton = document.querySelector("#postComment");
+
+if (postCommentButton) {
+    postCommentButton.addEventListener("click", postCommentHandler);
+};
